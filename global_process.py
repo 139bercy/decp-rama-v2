288ccf9c84f99bdcd9689ab7bc6d971821e03770
pierricspery <pierric.spery@hotmail.com>
2022-06-13 16:17:27 +0200

    fix : style, renommage de variables, séparation des process dans un dossier 'process'

--- global_process.py
+++ global_process.py
@@ -5,19 +5,19 @@ import json
 
 class GlobalProcess:
     def merge_all(self):
-        self.df = pd.concat(self.li_df, ignore_index=True)
+        self.df = pd.concat(self.dataframes, ignore_index=True)
 
     def drop_duplicate(self):
         pass
 
     def export_to_xml(self):
-        dico = {'marches': [{'marche': {k: v for k, v in m.items()
-                            if str(v) != 'nan'}} for m in self.df.to_dict(orient='records')]}
+        dico = {'marches': [{'marche': {k: v for k, v in m.items() if str(v) != 'nan'}}
+                            for m in self.df.to_dict(orient='records')]}
         with open("results/decp.xml", 'w') as f:
             f.write(dict2xml(dico))
 
     def export_to_json(self):
-        dico = {'marches': [{k: v for k, v in m.items()
-                            if str(v) != 'nan'} for m in self.df.to_dict(orient='records')]}
+        dico = {'marches': [{k: v for k, v in m.items() if str(v) != 'nan'}
+                            for m in self.df.to_dict(orient='records')]}
         with open("results/decp.json", 'w') as f:
             json.dump(dico, f, indent=2, ensure_ascii=False)
