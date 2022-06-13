124f27835a8ad10b5e2ea7d9393e4505b191e410
pierricspery <pierric.spery@hotmail.com>
2022-06-13 17:00:41 +0200

    fix : main, duplicate

---
+++ main.py
@@ -0,0 +1,21 @@
+# %%
+from process_factory import ProcessFactory
+from global_process import GlobalProcess
+
+
+
+# %%
+def main():
+    if __name__ == "__main__":
+        p = ProcessFactory()
+        p.getProcess()
+        gp = GlobalProcess()
+        gp.dataframes = p.dataframes
+        gp.merge_all()
+        gp.drop_duplicate()
+        gp.export_to_xml()
+        gp.export_to_json()
+
+
+# %%
+main()
