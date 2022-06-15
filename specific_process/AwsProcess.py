71e5e1f1af88ee211d8d3c9bcb95af043188da3b
AchazRyus <farcymarius@gmail.com>
2022-06-15 16:10:28 +0200

    style: supprimer def dans classe specifique

--- specific_process/AwsProcess.py
+++ specific_process/AwsProcess.py
@@ -6,12 +6,6 @@ class AwsProcess(SourceProcess):
         self.id = 1
         super().__init__()
 
-    def get(self):
-        super().get()
-
-    def convert(self):
-        super().convert()
-
     def fix(self):
         super().fix()
         # self.df['dureeMois'] = self.df['dureeMois'].astype(str)
