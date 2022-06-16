7e933b4ddc112a8f13f65affa3d915df5256328e
pierricspery <pierric.spery@hotmail.com>
2022-06-16 15:26:34 +0200

    fix : style, fix mineurs

--- specific_process/AwsProcess.py
+++ specific_process/AwsProcess.py
@@ -10,8 +10,18 @@ class AwsProcess(SourceProcess):
         # self.df['dureeMois'] = self.df['dureeMois'].astype(str)
         # self.df['montant'] = self.df['montant'].astype(str)
         # On se ramène au format souhaité pour titulaires, modifications et concessionnaires
-        self.df['modifications'] = [x if str(x) == 'nan' or str(x) == '[]' else ([{'modification':[y for y in x]}] if len(x)>1 else [{'modification':x[0]}]) for x in self.df['modifications']]
-        self.df['titulaires'] = [x if str(x) == 'nan' or str(x) == '[]' else ([{'titulaire':[y for y in x]}] if len(x)>1 else [{'titulaire':x[0]}]) for x in self.df['titulaires']]
+        self.df['modifications'] = [x if str(x) == 'nan' or str(x) == '[]'
+                                    else ([{'modification': [y for y in x]}] if len(x) > 1
+                                          else [{'modification': x[0]}])
+                                    for x in self.df['modifications']]
+        self.df['titulaires'] = [x if str(x) == 'nan' or str(x) == '[]'
+                                 else ([{'titulaire': [y for y in x]}] if len(x) > 1 else [{'titulaire': x[0]}])
+                                 for x in self.df['titulaires']]
+        self.df['concessionnaires'] = [x if str(x) == 'nan' or str(x) == '[]'
+                                       else ([{'concessionnaire': [y for y in x]}]
+                                             if len(x) > 1 else [{'concessionnaire': x[0]}])
+                                       for x in self.df['concessionnaires']]
+
         # cette étape permet de gérer les différences de formats entre XML et Json convertis en dataframe
         for x in self.df['modifications']:
             if len(x) > 0:
@@ -20,10 +30,10 @@ class AwsProcess(SourceProcess):
                     for i in range(len(y)):
                         if 'titulaires' in x[0]['modification'][i]:
                             z = x[0]['modification'][i]['titulaires']
-                            x[0]['modification'][i]['titulaires'] = (dict({'titulaire': [y for y in z]}) if len(z) > 1 else
-                                {'titulaire': z[0]})
+                            x[0]['modification'][i]['titulaires'] = \
+                                (dict({'titulaire': [y for y in z]}) if len(z) > 1 else {'titulaire': z[0]})
                 else:
                     if 'titulaires' in x[0]['modification']:
                         z = x[0]['modification']['titulaires']
-                        x[0]['modification']['titulaires'] = (dict({'titulaire': [y for y in z]}) if len(z) > 1 else
-                            {'titulaire': z[0]})
+                        x[0]['modification']['titulaires'] = \
+                            (dict({'titulaire': [y for y in z]}) if len(z) > 1 else {'titulaire': z[0]})
