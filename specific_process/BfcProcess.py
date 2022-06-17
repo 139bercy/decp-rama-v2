71cdb298f8f02f3c8d0cc5293f8fef5b27329925
pierricspery <pierric.spery@hotmail.com>
2022-06-17 11:49:14 +0200

    fix : ajout de fix dans BfcProcess

--- specific_process/BfcProcess.py
+++ specific_process/BfcProcess.py
@@ -1,7 +1,5 @@
 from general_process.source_process import SourceProcess
-from datetime import date
-from dateutil.relativedelta import relativedelta
-import pandas as pd
+import json
 
 
 class BfcProcess(SourceProcess):
@@ -15,4 +13,18 @@ class BfcProcess(SourceProcess):
         super().convert()
 
     def fix(self):
-        pass
+        super().fix()
+        self.df = self.df.drop(['contratTransverse'], axis=1)
+        self.df = self.df.drop(['donneesComplementaires'], axis=1)
+        # On enlève les OrderedDict et on se ramène au format souhaité
+        self.df['acheteur'] = self.df['acheteur'].apply(lambda x: json.loads(json.dumps(x)))
+        self.df['lieuExecution'] = self.df['lieuExecution'].apply(
+            lambda x: json.loads(json.dumps(x)))
+        self.df['titulaires'] = self.df['titulaires'].apply(
+            lambda x: json.loads(json.dumps(x)))
+        self.df['titulaires'] = self.df['titulaires'].apply(
+            lambda x: x if x is None or type(x) == list else [x])
+        self.df['modifications'] = self.df['modifications'].apply(
+            lambda x: x if x is None or str(x) == 'nan' else json.loads(json.dumps(x)))
+        self.df['modifications'] = self.df['modifications'].apply(
+            lambda x: x if type(x) == list else [] if x is None or str(x) == 'nan' else [x])
