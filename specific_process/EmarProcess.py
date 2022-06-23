949f1e54f2920cbed8d499a275d48e99a02872d2
pierricspery <pierric.spery@hotmail.com>
2022-06-23 16:46:27 +0200

    fix : fix et convert de emar et megalis

--- specific_process/EmarProcess.py
+++ specific_process/EmarProcess.py
@@ -12,8 +12,29 @@ class EmarProcess(SourceProcess):
         super().get()
 
     def convert(self):
-        pass
+        super().convert()
 
     def fix(self):
-        pass
+        super().fix()
+        self.df['modifications'] = [x if str(x) == 'nan' or str(x) == '[]'
+                                    else ([{'modification': [y for y in x]}] if len(x) > 1
+                                          else [{'modification': x[0]}])
+                                    for x in self.df['modifications']]
+        self.df['titulaires'] = [x if str(x) == 'nan' or str(x) == '[]'
+                                 else ([{'titulaire': [y for y in x]}] if len(x) > 1 else [{'titulaire': x[0]}])
+                                 for x in self.df['titulaires']]
 
+        for x in self.df['modifications']:
+            if len(x) > 0:
+                y = x[0]['modification']
+                if type(y) is list:
+                    for i in range(len(y)):
+                        if 'titulaires' in x[0]['modification'][i]:
+                            z = x[0]['modification'][i]['titulaires']
+                            x[0]['modification'][i]['titulaires'] = \
+                                (dict({'titulaire': [y for y in z]}) if len(z) > 1 else {'titulaire': z[0]})
+                else:
+                    if 'titulaires' in x[0]['modification']:
+                        z = x[0]['modification']['titulaires']
+                        x[0]['modification']['titulaires'] = \
+                            (dict({'titulaire': [y for y in z]}) if len(z) > 1 else {'titulaire': z[0]})
