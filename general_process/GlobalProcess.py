183f4d161f513da982449abbfd8dfe5612b13dbc
pierricspery <pierric.spery@hotmail.com>
2022-06-30 13:04:13 +0200

    fix : suppression commentaire

--- general_process/GlobalProcess.py
+++ general_process/GlobalProcess.py
@@ -63,14 +63,14 @@ class GlobalProcess:
 
     def export(self):
         logging.info("  ÉTAPE EXPORTATION")
-        '''logging.info("Début de l'étape Exportation en XML")
+        logging.info("Début de l'étape Exportation en XML")
         dico = {'marches': [{'marche': {k: v for k, v in m.items() if str(v) != 'nan'}}
                             for m in self.df.to_dict(orient='records')]}
         with open("results/decp.xml", 'w') as f:
             f.write(dict2xml(dico))
         xml_size = os.path.getsize(r'results/decp.xml')
         logging.info("Exportation XML OK")
-        logging.info(f"Taille de decp.xml : {xml_size}")'''
+        logging.info(f"Taille de decp.xml : {xml_size}")
         logging.info("Début de l'étape Exportation en JSON")
         dico = {'marches': [{k: v for k, v in m.items() if str(v) != 'nan'}
                             for m in self.df.to_dict(orient='records')]}
