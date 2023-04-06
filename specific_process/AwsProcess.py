7d4d7a4b1ebfa28658b3df21e4372d7666650024
kaa-serpent <guillaumedecadoudal@gmail.com>
2023-04-06 10:57:46 +0200

    fix : variable name

--- specific_process/AwsProcess.py
+++ specific_process/AwsProcess.py
@@ -26,24 +26,24 @@ class AwsProcess(SourceProcess):
                                        for x in self.df['concessionnaires']]
 
         # cette étape permet de gérer les différences de formats entre XML et Json convertis en dataframe
-        for x in self.df['modifications']:
-            if len(x) > 0:
-                y = x[0]['modification']
-                if type(y) is list:
-                    for i in range(len(y)):
-                        if 'titulaires' in x[0]['modification'][i]:
-                            z = x[0]['modification'][i]['titulaires']
-                            if not z : # Certains nouveaux formats de données renvoient des listes vides de modifs de titulaires.
+        for modification_dict in self.df['modifications']:
+            if len(modification_dict) > 0:
+                modifications = modification_dict[0]['modification']
+                if type(modifications) is list:
+                    for i in range(len(modifications)):
+                        if 'titulaires' in modification_dict[0]['modification'][i]:
+                            modifs_titulaire = modification_dict[0]['modification'][i]['titulaires']
+                            if not modifs_titulaire : # Certains nouveaux formats de données renvoient des listes vides de modifs de titulaires.
                                 print("Liste titulaire vide pour l'entrée suivante : \n")
-                                print(y)
+                                print(modifications)
                             else:
-                                x[0]['modification'][i]['titulaires'] = \
-                                    ([y for y in z]) if len(z) > 1 else [z[0]]
+                                modification_dict[0]['modification'][i]['titulaires'] = \
+                                    ([modifications for modifications in modifs_titulaire]) if len(modifs_titulaire) > 1 else [modifs_titulaire[0]]
                 else:
-                    if 'titulaires' in x[0]['modification']:
-                        z = x[0]['modification']['titulaires']
+                    if 'titulaires' in modification_dict[0]['modification']:
+                        modifs_titulaire = modification_dict[0]['modification']['titulaires']
                         # Dans les données si il y a un champ titulaire vide, ça crash. Corrigeons ça
-                        if z != []:
-                            x[0]['modification']['titulaires'] = \
-                                ([y for y in z]) if len(z) > 1 else [z[0]]
+                        if modifs_titulaire:
+                            modification_dict[0]['modification']['titulaires'] = \
+                                ([modifications for modifications in modifs_titulaire]) if len(modifs_titulaire) > 1 else [modifs_titulaire[0]]
                         # Sinon on fait rien car le champ titulaire est vide.
